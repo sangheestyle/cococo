f23377fbf2196cc8cbc0fb147d62376e7dff9b77
Sanghee Kim <sanghee.kim@zalando.fi>
2019-12-31 15:06:00 +0900

    We have a fake model now
    load, fit, and predict are our elements of our standard model.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+class ModelA:
+  """This is a sort of prototype implementation for our base model."""
+  def __init__(self):
+    pass
+
+  def load(self):
+    print("ModelA: loaded")
+
+  def fit(self):
+    print("ModelA: fitted")
+
+  def predict(self):
+    print("ModelA: predicted")
+
+
+m = ModelA()
+m.load()
+m.fit()
+m.predict()
