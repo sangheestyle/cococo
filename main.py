239e8fb3fb83191fb6197b87890c5c04873965fa
Sanghee Kim <sanghee.kim@zalando.fi>
2019-12-31 15:45:42 +0900

    use ABC

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-class ModelA:
+from abc import ABC, abstractmethod
+
+
+class FMRIModelBase(ABC):
+
+    def load(self, data):
+      print(f"Model: {len(data)} items are loaded.")
+    
+    @abstractmethod
+    def fit(self):
+      pass
+
+    @abstractmethod
+    def predict(self):
+      pass
+
+class ModelA(FMRIModelBase):
   """This is a sort of prototype implementation for our base model."""
   def __init__(self):
     pass
-
-  def load(self):
-    print("ModelA: loaded")
 
   def fit(self):
     print("ModelA: fitted")
@@ -13,13 +26,10 @@ class ModelA:
     print("ModelA: predicted")
 
 
-class ModelB:
+class ModelB(FMRIModelBase):
   """This is a sort of prototype implementation for our base model."""
   def __init__(self):
     pass
-
-  def load(self):
-    print("ModelB: loaded")
 
   def fit(self):
     print("ModelB: fitted")
@@ -28,12 +38,20 @@ class ModelB:
     print("ModelB: predicted")
 
 
+
+data = [1, 2, 3]
+
 ma = ModelA()
-ma.load()
+ma.load(data)
 ma.fit()
 ma.predict()
 
 mb = ModelB()
-mb.load()
+mb.load(data)
 mb.fit()
 mb.predict()
+
+# mc = ModelC()
+# mc.load()
+# mc.fit()
+# mc.predict()
